8a9c247d5df3427a63275b91f809682c717c75fd
BryanAzuero <beazuero@espe.edu.ec>
2021-01-06 14:11:05 -0500

    initial commit

---
+++ ws03/BillingSystem_V1/src/ec/edu/espe/BillingSystem/model/Check.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.BillingSystem.model;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class Check {
+    String withdraws;
+    int idnumber;
+    int value;
+    
+    void verifyfunds(){
+        
+    }
+}
---
+++ ws03/BillingSystem_V1/src/ec/edu/espe/BillingSystem/model/InvoiceDetail.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.BillingSystem.model;
+
+/**
+ *
+ * @author USUARIO
+ */
+public class InvoiceDetail {
+    int billcode;
+    int articlecode;
+    int amount;
+    float iva;
+    int saleprice;
+    void datacenter(Person person){
+        
+    }
+    void totalcalculate(Bill bill){
+        
+    }
+    void showbilldetail(){
+        
+    }
+}
